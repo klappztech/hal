892b6effbdeae416c2ee07495f96c28bb0444940
klappztech <klappztech@gmail.com>
2020-03-26 23:53:25 +0530

    just html log

--- log.php
+++ log.php
@@ -14,13 +14,9 @@ $search_result = $db->getAlllog();
 
 <head>
    <meta name="viewport" content="width = device-width, initial-scale = 1">
-   <link rel="stylesheet" href="themes/android_blue.min.css" />
-   <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
-   <link rel="stylesheet" href="themes/jquery.mobile.structure-1.4.5.min.css" />
    <link rel="stylesheet" type="text/css" href="./css/style.css">
 
    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-   <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
 
 </head>
@@ -31,21 +27,26 @@ $search_result = $db->getAlllog();
 
 
       <div data-role="header" data-position="fixed" data-theme="b">
-         <a href="search.php" data-icon="back" class="ui-btn-left">Back</a>
+         <a href="search.php" data-icon="back" class="ui-btn-left">[Home]</a>
+         <a href="logout.php" data-icon="power" class="ui-btn-right">[Logout]</a>
+
          <h1>Log</h1>
-         <a href="logout.php" data-icon="power" class="ui-btn-right">Logout</a>
       </div>
 
       <div data-role="main" class="ui-content" id="log_container">
          <script>
             $(document).ready(function() {
-               $("#log_container").load('log_list.php');
+               var url = window.location.pathname;
+               var filename = url.substring(url.lastIndexOf('/') + 1);
 
-               setInterval(function() {
-                    $("#log_container").load('log_list.php');
-                  //location.reload();
+                  $("#log_container").load('log_list.php');
 
-               }, 2000);
+                  setInterval(function() {
+                     $("#log_container").load('log_list.php');
+                     //location.reload();
+
+                  }, 2000);
+               
             });
          </script>
 
